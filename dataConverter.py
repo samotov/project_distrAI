b79c6dc7dec95970541c30510295bf531e2a50df
rienvancampenhout <rien1van2campenhout3@gmail.com>
2024-10-23 11:57:21 +0200

    Adds some error messages and cleans up some code

--- dataConverter.py
+++ dataConverter.py
@@ -60,46 +60,51 @@ class DataConverter:
         seg_image_files = [f for f in os.listdir(seg_folder) if f.endswith(('.jpg', '.png', '.jpeg'))]
         normal_image_files = [f for f in os.listdir(normal_folder) if f.endswith(('.jpg', '.png', '.jpeg'))]
 
-        # We define how many training images we have
-        num_training_images = int(len(seg_image_files)*train_percentage)
-
-        # We assume that the images have the same index at the end
-        for image_index in range(len(seg_image_files)):
-            # We define wether we want this image to be a train image or a validation image
-            mode = 'train' if image_index < num_training_images else 'val'
-
-            # We identify all the paths that we need
-            seg_image_path = os.path.join(seg_folder, seg_image_files[image_index])
-            normal_image_input_path = os.path.join(normal_folder, normal_image_files[image_index])
-            normal_image_output_path = os.path.join('datasets', self.output_dir, 'images', mode, str(current_index) + '.png')
-            labels_ouput_path = os.path.join('datasets', self.output_dir, 'labels', mode, str(current_index) + '.txt')
-
-            # We read the image get the width and the height and write the normal image to the correct path
-            normal_image = cv2.imread(normal_image_input_path)
-            height, width, _ = normal_image.shape
-            cv2.imwrite(normal_image_output_path, normal_image)
-
-            # We get the bounding boxes from the image
-            bounding_boxes = self.get_2D_bounding_box_from_segmeted_image(seg_image_path)
-
-            # We write the bounding box data in the correct format to the .txt file
-            with open(labels_ouput_path, 'w') as file:
-                class_index = 0
-                for _ , bboxes in bounding_boxes.items():
-                    if len(bboxes) != 0:
-                        for bbox in bboxes:
-                            # We normalize the bbox values
-                            x, y, w, h = bbox
-                            norm_x = x / width
-                            norm_y = y / height
-                            norm_w = w / width
-                            norm_h = h / height
-
-                            # We define the string and write it to the .txt file
-                            bbox_string = str(class_index) + ' ' + str(norm_x) + ' ' + str(norm_y) + ' ' + str(norm_w) + ' ' + str(norm_h)
-                            file.write(bbox_string + '\n')
-                    class_index += 1
-            current_index += 1
+        # we check wether the amount of images is the same
+        if len(seg_image_files) != len(normal_image_files):
+            print('Error in folder ' + self.input_dir + ': Amount of segmented and normal images should be the same!')
+        else:
+            # We define how many training images we have
+            num_training_images = int(len(seg_image_files)*train_percentage)
+    
+            # We assume that the images have the same index at the end
+            for image_index in range(len(seg_image_files)):
+                # We define wether we want this image to be a train image or a validation image
+                mode = 'train' if image_index < num_training_images else 'val'
+    
+                # We identify all the paths that we need
+                seg_image_path = os.path.join(seg_folder, seg_image_files[image_index])
+                normal_image_input_path = os.path.join(normal_folder, normal_image_files[image_index])
+                normal_image_output_path = os.path.join('datasets', self.output_dir, 'images', mode, str(current_index) + '.png')
+                labels_ouput_path = os.path.join('datasets', self.output_dir, 'labels', mode, str(current_index) + '.txt')
+    
+                # We read the image get the width and the height and write the normal image to the correct path
+                normal_image = cv2.imread(normal_image_input_path)
+                height, width, _ = normal_image.shape
+                cv2.imwrite(normal_image_output_path, normal_image)
+    
+                # We get the bounding boxes from the image
+                bounding_boxes = self.get_2D_bounding_box_from_segmeted_image(seg_image_path)
+    
+                # We write the bounding box data in the correct format to the .txt file
+                with open(labels_ouput_path, 'w') as file:
+                    class_index = 0
+                    for _ , bboxes in bounding_boxes.items():
+                        if len(bboxes) != 0:
+                            for bbox in bboxes:
+                                # We normalize the bbox values
+                                x, y, w, h = bbox
+                                norm_x = x / width
+                                norm_y = y / height
+                                norm_w = w / width
+                                norm_h = h / height
+    
+                                # We define the string and write it to the .txt file
+                                bbox_string = str(class_index) + ' ' + str(norm_x) + ' ' + str(norm_y) + ' ' + str(norm_w) + ' ' + str(norm_h)
+                                file.write(bbox_string + '\n')
+                        class_index += 1
+                current_index += 1
+            print('Succesfully converted the data!')
         return current_index
 
 
--- main.py
+++ main.py
@@ -30,13 +30,18 @@ def main():
                 'traffic signs': [np.array([220, 220, 0]), 1, 150],
                 'traffic lights': [np.array([250, 170, 30]), 1, 150]}
     
-    data_converter = DataConverter.DataConverter("captured_data/noon", "test", class_color_info_map)
+    data_converter_noon = DataConverter.DataConverter("captured_data/noon", "test2", class_color_info_map)
+    data_converter_cloudy_noon = DataConverter.DataConverter("captured_data/cloudy_noon", "test2", class_color_info_map)
+    data_converter_cloudy_night = DataConverter.DataConverter("captured_data/cloudy_night", "test2", class_color_info_map)
 
-    data_converter.convert_data(1, 0.8)
+    next_index = data_converter_noon.convert_data(1, 0.8)
+    next_index = data_converter_cloudy_noon.convert_data(next_index, 0.8)
+    data_converter_cloudy_night.convert_data(next_index, 0.8)
 
     yolomodel = ObjectLocalizationModel.ObjectLocalizationModel()
-    results = yolomodel.train_model('test.yaml', 50, 16)
-    results.show()
+    
+    # results = yolomodel.train_model('test.yaml', 3, 16)
+    # results.show()
     
     
 
